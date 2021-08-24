7d1935196455285e310ceeaa4bbc1b1ce3fee5e9
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-08-24 11:57:50 +0200

    Storage Flysystem with aws

---
+++ src/Controller/Admin/FichierCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Fichier;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class FichierCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Fichier::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('nom'),
+            TextField::new('imageFile', 'Upload')
+                ->setFormType(VichImageType::class)
+                ->onlyOnForms(),
+            ImageField::new('imageName', 'Fichier')
+                ->setBasePath('http://localhost:9000/fichier/')
+                ->hideOnForm()
+            
+        ];
+    }
+}
